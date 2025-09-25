ae13ef7d4f99f9fe64237a10e307ada2fdc14a35
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-25 21:11:46 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,18 @@
 # messaging_app/chats/urls.py
 
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 
-# Création du router
+# Router principal
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+# Router imbriqué : messages liés à une conversation
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversations_router.urls)),
 ]
