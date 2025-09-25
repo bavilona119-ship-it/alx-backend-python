8b92bc101972725534e22f2dca991e08239bee40
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-25 21:17:57 +0200

    Delete messaging_app/urls.py

--- messaging_app/urls.py
+++
@@ -1,9 +0,0 @@
-# messaging_app/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('messaging_app.chats.urls')),  # <-- Ajout des routes API
-]
