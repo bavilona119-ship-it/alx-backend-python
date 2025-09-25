06627aa7d354e83dcec9d33df9dfa306f1211ed8
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-25 21:17:24 +0200

    Create urls.py

---
+++ messaging_app/messaging_app/urls.py
@@ -0,0 +1,9 @@
+# messaging_app/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('messaging_app.chats.urls')),  # inclusion des routes API
+]
