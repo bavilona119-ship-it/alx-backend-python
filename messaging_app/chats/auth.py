8f3e219aedf82d8762bfccf468aa61c916a5b97c
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-29 14:13:23 +0200

    Update auth.py

--- messaging_app/chats/auth.py
+++ messaging_app/chats/auth.py
@@ -1,1 +1,13 @@
+# messaging_app/chats/auth.py
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from django.urls import path
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
