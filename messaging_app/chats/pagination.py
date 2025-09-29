8d2f95f3192ed86236bda19271f91a2ac8185c00
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-29 14:42:14 +0200

    Update pagination.py

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,1 +1,17 @@
+# chats/pagination.py
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+class MessagePagination(PageNumberPagination):
+    page_size = 20  # nombre de messages par page
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
 
