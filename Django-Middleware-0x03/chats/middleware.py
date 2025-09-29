844d8771e8807037f4ed27b2c80c1a1adf3e0bcb
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-29 15:56:25 +0200

    Create middleware.py

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,20 @@
+import logging
+from datetime import datetime
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # Configure logger
+        logging.basicConfig(
+            filename="requests.log",  # log file at project root
+            level=logging.INFO,
+            format="%(message)s"
+        )
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logging.info(log_message)
+
+        response = self.get_response(request)
+        return response
