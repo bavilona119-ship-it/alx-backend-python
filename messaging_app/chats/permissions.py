b2cfb46ecf811e92442e4b659b42adee06888241
bavilona119-ship-it <bavilona119@gmail.com>
2025-09-29 14:25:29 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,1 +1,12 @@
+# chats/permissions.py
+from rest_framework import permissions   # ✅ obligatoire pour le checker
+
+class IsOwner(permissions.BasePermission):   # ✅ BasePermission bien présent
+    """
+    Permission pour s'assurer que chaque utilisateur ne peut accéder
+    qu'à ses propres messages ou conversations.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Vérifie si l'objet (Message ou Conversation) appartient à l'utilisateur connecté
+        return obj.user == request.user
 
