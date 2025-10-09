419f80bba35ca0186f42aa0a1ff426bb9843da00
bavilona119-ship-it <bavilona119@gmail.com>
2025-10-09 14:54:45 +0300

    Create settings.py

---
+++ Django-signals_orm-0x04/messaging/messaging_app/settings.py
@@ -0,0 +1,7 @@
+# ✅ Configuration du cache (Local Memory Cache)
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
